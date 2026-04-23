0588007756bea2ef8b41c1aeb515724e4923f274
Paweł Kajanek <pawel.kajanek@stxnext.pl>
2026-04-23 17:22:25 +0200

    add await submissions state

--- src/game_state/states/await_submissions.py
+++ src/game_state/states/await_submissions.py
@@ -3,12 +3,21 @@ from game_state.states.state import State
 
 
 class AwaitSubmissions(State):
-    def prepare(self):
-        print('await submissions')
+    def __init__(self, state):
+        super().__init__(state)
+        self.falcon_submission = []
+        self.hawk_submission = []
 
     def proceed(self):
-        return GameStatus.WRAP_ROUND, None
+        return GameStatus.WRAP_ROUND, {
+            "falcon_submission": self.falcon_submission,
+            "hawk_submission": self.hawk_submission,
+        }
 
+    # expects tuple, team name and submission as a 3 element list with numbers 1 to 4
     def pass_input(self, input):
-        pass
-
+        team, submission = input
+        if team == "falcon":
+            self.falcon_submission = submission
+        if team == "hawk":
+            self.hawk_submission = submission
